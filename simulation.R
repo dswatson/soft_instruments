dbc42185e5447b7c9a0976fa731bf85ddc464c2b
David Watson <david.s.watson11@gmail.com>
2021-09-13 07:49:59 +0200

    grid

--- simulation.R
+++ simulation.R
@@ -1,11 +1,24 @@
+# Set working directory
+setwd('~/Documents/UCL/soft_instruments')
+
 # Load libraries, register cores
 library(data.table)
 library(MultiRNG)
+library(tidyverse)
 library(doMC)
 registerDoMC(8)
 
 # Set seed
 set.seed(123, kind = "L'Ecuyer-CMRG")
+
+# Define simulation grid
+sim_idx <- expand_grid(
+  z_rho = c(0, 0.5),
+  rho = c(0.25, 0.75, -0.25, -0.75),
+  pr_valid = c(0, 0.25, 0.5)
+) %>% 
+  mutate(idx = row_number()) %>%
+  select(idx, z_rho, rho, pr_valid)
 
 #' @param n Sample size.
 #' @param d_z Dimensionality of Z.
@@ -16,15 +29,15 @@ set.seed(123, kind = "L'Ecuyer-CMRG")
 #' @param alpha Average treatment effect of X on Y.
 #' @param r2_x Proportion of variance explained for X.
 #' @param r2_y Proportion of variance explained for Y.
-#' @param prop_valid Proportion of candidate instruments that are valid, 
+#' @param pr_valid Proportion of candidate instruments that are valid, 
 #'   i.e. have no direct effect on Y.
+#' @param s_idx Simulation index
 
 # Simulate data (inspired by Hartwig et al., 2017)
-sim_dat <- function(n, d_z, z_cnt, z_rho,
-                    rho, alpha, r2_x, r2_y, prop_valid) {
+sim_dat <- function(n, d_z, z_cnt, z_rho, rho, alpha, r2_x, r2_y, pr_valid, s_idx) {
   # What proportion are valid?
-  valid_cnt <- round(prop_valid * d_z)
-  prop_valid <- valid_cnt / d_z
+  valid_cnt <- round(pr_valid * d_z)
+  pr_valid <- valid_cnt / d_z
   # Draw Z's
   if (z_rho != 0) {
     Sigma_z <- toeplitz(z_rho^(0:(d_z - 1)))
@@ -32,7 +45,7 @@ sim_dat <- function(n, d_z, z_cnt, z_rho,
   if (z_cnt) {
     z <- matrix(rnorm(n * d_z), ncol = d_z)
     if (z_rho == 0) {
-      Sigma_z < diag(rep(1, d_z))
+      Sigma_z <- diag(rep(1, d_z))
     } else {
       z <- z %*% chol(Sigma_z)
     }
@@ -64,8 +77,8 @@ sim_dat <- function(n, d_z, z_cnt, z_rho,
   # And again for Y, although we need to solve a quadratic equation for eta_y
   nu2 <- runif(d_z, min = 0.01, max = 0.2)
   sigma_zy <- sd(sqrt(0.1) * as.numeric(z %*% nu2))
-  gamma <- (sqrt(0.1) / sigma_zy) * nu2 * (1 - prop_valid)
-  if (prop_valid > 0) {
+  gamma <- (sqrt(0.1) / sigma_zy) * nu2 * (1 - pr_valid)
+  if (pr_valid > 0) {
     gamma[sample(d_z, valid_cnt)] <- 0
   }
   var_mu <- as.numeric(t(gamma) %*% Sigma_z %*% gamma) + alpha^2 * var_x + 
@@ -74,20 +87,24 @@ sim_dat <- function(n, d_z, z_cnt, z_rho,
   b <- 2 * alpha * rho * eta_x
   eta_y <- (-b + sqrt(b^2 - 4 * (var_mu - var_y))) / 2
   y <- as.numeric(z %*% gamma) + x * alpha + eps[, 2] * eta_y
+  # Export results
   dat <- data.table(z, 'x' = x, 'y' = y)
+  fwrite(dat, paste0('./simulations/sim', s_idx, '.csv'))
   params <- list(
     'alpha' = alpha, 'beta' = beta, 'gamma' = gamma, 'eta_x' = eta_x, 
     'eta_y' = eta_y, 'rho' = rho, 'r2_x' = r2_x, 'r2_y' = r2_y
   )
-  out <- list('dat' = dat, 'params' = params)
-  return(out)
+  saveRDS(params, paste0('./simulations/params', s_idx, '.rds'))
 }
-# For example:
-sim <- sim_dat(n = 1e4, d_z = 5, z_cnt = 6, z_rho = 0.25, 
-               rho = 0.25, alpha = 1, r2_x = 0.5, r2_y = 0.5, prop_valid = 0.5)
-head(sim$dat)
-sim$params
+# Execute in parallel
+foreach(aa = sim_idx$idx) %dopar%
+  sim_dat(n = 1e4, d_z = 8, z_cnt = TRUE, z_rho = sim_idx$z_rho[aa],
+          rho = sim_idx$rho[aa], alpha = 1, r2_x = 0.5, r2_y = 0.5, 
+          pr_valid = sim_idx$pr_valid[aa], aa)
+
+# And also
+fwrite(sim_idx, './simulations/sim_idx.csv')
 
 
-
-
+# Problem: pr_z imposes some constraints on Sigma_z in the binomial case,
+# as correlations cannot exceed upper limits imposed by expectations
